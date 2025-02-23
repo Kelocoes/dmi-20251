90b120bd28f95c2b889c8028f0370929862e2562
kelocoes <kevin.rodriguez109@gmail.com>
2025-02-22 19:32:50 -0500

    Update weeks

--- week-1/main.js
+++ week-1/main.js
@@ -262,3 +262,12 @@ function createSuperHero () {
 }
 
 createSuperHero();
+
+// Deconstruction
+// const person = {
+//     name: "Alice",
+//     age: 25
+// }
+
+// const { name, age } = person;
+// console.log(name); // Alice
