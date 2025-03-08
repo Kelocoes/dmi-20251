148fffd87c34c2b1c9d78476eaa2a7febe70bb01
kelocoes <kevin.rodriguez109@gmail.com>
2025-03-08 16:34:29 -0500

    Update structure project week-6

--- weekly/week-6/src/components/Card/Card.js
+++ weekly/week-6/src/components/Card/Card.js
@@ -14,6 +14,23 @@ class Card extends HTMLElement {
 
     render() {
         this.shadowRoot.innerHTML = `
+            <style>
+                div {
+                    border: 1px solid #ccc;
+                    border-radius: 8px;
+                    padding: 16px;
+                    text-align: center;
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                }
+                p {
+                    margin: 8px 0;
+                    font-family: Arial, sans-serif;
+                }
+                img {
+                    max-width: 100%;
+                    border-radius: 8px;
+                }
+            </style>
             <div>
                 <p>ID: ${this.getAttribute('id')}</p>
                 <p>Name: ${this.getAttribute('name')}</p>
--- weekly/week-6/src/index.js
+++ weekly/week-6/src/index.js
@@ -1,9 +1,6 @@
 import Card from './components/Card/Card.js';
-import ListRickCharsComponent from './layouts/ListRickCharsComponent.js';
+import RickList from './layouts/RickList/RickList.js';
 
 // console.log('Custom element defined');
-customElements.define('rick-list', ListRickCharsComponent);
+customElements.define('rick-list', RickList);
 customElements.define('custom-card', Card);
-
-document.querySelector('my-first-component').setAttribute('example-attribute', 'Hello World');
-document.querySelector('my-first-component').removeAttribute('example-attribute');
