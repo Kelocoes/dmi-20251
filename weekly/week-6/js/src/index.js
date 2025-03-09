8f65d99db6e8333e4d877cc5b82b2e649b4838d4
kelocoes <kevin.rodriguez109@gmail.com>
2025-03-08 19:02:53 -0500

    Update and reorganize

---
+++ weekly/week-6/js/src/index.js
@@ -0,0 +1,6 @@
+import Card from './components/Card/Card.js';
+import RickList from './layouts/RickList/RickList.js';
+
+// console.log('Custom element defined');
+customElements.define('rick-list', RickList);
+customElements.define('custom-card', Card);
