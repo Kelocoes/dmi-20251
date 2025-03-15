e0d5f6813b916f020a59283b81ba1dbb552eb75f
kelocoes <kevin.rodriguez109@gmail.com>
2025-03-15 15:07:14 -0500

    Guide with ts

--- weekly/week-6/js/src/components/Card/Card.js
+++ weekly/week-6/js/src/components/Card/Card.js
@@ -1,11 +1,11 @@
-class Card extends HTMLElement {
+class Noticia extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
     }
 
     static get observedAttributes() {
-        return ['id', 'name', 'status', 'image'];
+        return ['id', 'title', 'description'];
     }
 
     connectedCallback() {
@@ -13,34 +13,22 @@ class Card extends HTMLElement {
     }
 
     render() {
-        if (this.shadowRoot) {
-            this.shadowRoot.innerHTML = `
-                <style>
-                    div {
-                        border: 1px solid #ccc;
-                        border-radius: 8px;
-                        padding: 16px;
-                        margin: 10px;
-                        text-align: center;
-                        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-                    }
-                    p {
-                        margin: 8px 0;
-                        font-family: Arial, sans-serif;
-                    }
-                    img {
-                        max-width: 100%;
-                        border-radius: 8px;
-                    }
-                </style>
-                <div>
-                    <p>ID: ${this.getAttribute('id')}</p>
-                    <p>Name: ${this.getAttribute('name')}</p>
-                    <p>Status: ${this.getAttribute('status')}</p>
-                    <img src="${this.getAttribute('image')}" alt="${this.getAttribute('name')}">
-                </div>
-            `;
-        }
+        this.shadowRoot.innerHTML = `
+            <style>
+                div {
+                    background-color: #f1f1f1;
+                    border: 1px solid #e5e5e5;
+                    border-radius: 5px;
+                    margin: 10px;
+                    padding: 10px;
+                }
+            </style>
+            <div>
+                <p>ID: ${this.getAttribute('id')}</p>
+                <p>Name: ${this.getAttribute('title')}</p>
+                <p>Status: ${this.getAttribute('description')}</p>
+            </div>
+        `;
     }
 }
 
---
+++ weekly/week-7/project-demo/src/utils/prueba.ts
@@ -0,0 +1,5 @@
+function logDePrueba() {
+  console.log('Hola mundo desde un modulo');
+}
+
+export { logDePrueba };
